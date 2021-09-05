683c74c2e4e4cfbde6b2ab78701cc8b7c51d5919
Ben Schlegel <brschlegels@gmail.com>
2021-09-05 19:18:40 -0400

    commit

--- MultiMouse/Scene.h
+++ MultiMouse/Scene.h
@@ -7,14 +7,12 @@
 #include "FuncTimerFunctions.h"
 #include <string>
 #define SFML_STATIC
-#define BOOST_USE_WINDOWS_H
 #include <iostream>
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
 #include "SFML/OpenGL.hpp"
 #include "ShapeManager.h"
 #include "Delegate.h"
-#include <boost/signals2.hpp>
 #include "Structure.h"
 
 class CollisionManager;
@@ -45,5 +43,4 @@ public:
 	Delegate<void> initDel;
 
 	void startFuncTimers();
-	boost::signals2::signal<void()> initSignal;
 };
